fd03668607843bb9fbf057e5e1d25f8506b75272
David Fairbairn <davidtfairbairn@gmail.com>
2017-06-29 13:04:16 -0600

    Added a stub and improved some commenting/docstrings. Ultimately uptime will absorb some of the more high-level 'fetch this' types of functions that are still currently in rawacf_metadata

--- uptime.py
+++ uptime.py
@@ -1,9 +1,14 @@
 """
 file: 'uptime.py'
 description:
-    This script is to be used to calculate uptimes and downtimes for 
-    SuperDARN arrays by analyzing the metadata entries in the 
-    superdarntimes.sqlite database.
+    This file (currently) contains the methods that can be used to 
+    calculate SuperDARN experiment statistics by manipulating the
+    sqlite database constructed using methods from 
+    'rawacf_metadata.py'.
+
+    Ultimately, this file will work as a front-end for users, so 
+    high-level methods to fetch and process data will be moved into
+    here to sit alongside the statistics methods. 
 
 author: David Fairbairn
 date: June 20 2017
@@ -25,10 +30,21 @@ logging.basicConfig(level=logging.INFO)
 # -----------------------------------------------------------------------------
 #                           POST PROCESSING METHODS
 # -----------------------------------------------------------------------------
-def stats_day(date_str, cur):
+
+def stats_day(date_str, cur, stid=5):
     """
-    
+    Performs a database query, reporting the % usage of a SuperDARN 
+    array on a particular day. 
+
+    *** PARAMS ***
+        date_str (string): a string in the format "20160418" yyyymmdd
+        cur (sqlite3 cursor): cursor into an sqlite3 database
+        stid (int): the station ID for the SuperDARN array desired
+                see here: http://superdarn.ca/news/item/58-sd-radar-list
     """
+
+    # TODO: Finish this!
+
     import heapq
     sql = 'select * from exps where start_date regexp "%s" or end_date regexp "%s"'
     schedule = []
@@ -55,7 +71,13 @@ def stats_day(date_str, cur):
             pass
             #durations.append(r.end_time - r.start_time)
     # sum the intervals in durations and divide by 24hr*3600s/hr
-    return
+    return None
+
+def stats_month(date_str, cur, stid=5):
+    """
+    As above so below baby
+    """
+    return None
 
 if __name__ == "__main__":
     import rawacf_metadata as rdat
